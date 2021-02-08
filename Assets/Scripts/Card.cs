3c4f6dfed74dd1a4cb72771a397f03239ddb86c4
Shelby Parrish <shanneparrish@gmail.com>
2021-02-08 18:16:45 -0500

    Card Work
    Created class to read in card info from text file, added card attributes (name, cost, etc...) to Card class

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    //Information about the cards
+    [SerializeField]
+    string name;
+    [SerializeField]
+    int id = 0;
+    [SerializeField]
+    int mana = 0;
+    [SerializeField]
+    int damage = 0;
+
     // Start is called before the first frame update
     void Start()
     {
