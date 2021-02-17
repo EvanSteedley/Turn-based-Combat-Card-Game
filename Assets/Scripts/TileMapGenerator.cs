ff03e323d6ee8fe29f96c4b7d718974d315fd751
EvanSteedley <43506468+EvanSteedley@users.noreply.github.com>
2021-02-17 18:22:51 -0500

    Added tiles to 2D array, created Wall formula
    I fixed an error with the logic adding the tiles to a 2D array for easy access.  I also created an algorithm that places "Wall Units" around the outer perimeter of the grid.  The y-value of its position is the wall's y-scale divided by 2, because this places its position at the exact height so that the bottom of the Wall model is at y=0.

--- Assets/Scripts/TileMapGenerator.cs
+++ Assets/Scripts/TileMapGenerator.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class TileMapGenerator : MonoBehaviour
 {
     [SerializeField] GameObject TilePrefab;
+    [SerializeField] GameObject WallPrefab;
 
     [SerializeField] int tileWidth = 10;
     [SerializeField] int tileLength = 10;
     [SerializeField] float tileOffset = 1.1f;
 
-    [SerializeField]
-    GameObject[,] Tiles;
+    Tile[,] Tiles;
 
     // Start is called before the first frame update
     void Start()
     {
+        Tiles = new Tile[tileWidth, tileLength];
         CreateTileGrid();
-        Tiles = new GameObject[tileWidth, tileLength];
+        
     }
 
     // Update is called once per frame
@@ -28,15 +29,37 @@ public class TileMapGenerator : MonoBehaviour
 
     public void CreateTileGrid()
     {
+        
         for (int x = 0; x < tileWidth; x++)
         {
             for (int y = 0; y < tileLength; y++)
             {
-                GameObject tempTile = Instantiate(TilePrefab);
+                GameObject tempTile = Instantiate(TilePrefab, this.transform) as GameObject;
                 tempTile.transform.position = new Vector3(x * tileOffset, 0, y * tileOffset);
-                tempTile.transform.parent = this.transform;
-                tempTile.name = x.ToString() + ", " + y.ToString();
-                
+                if(x == 0)
+                {
+                    GameObject tempWall = Instantiate(WallPrefab, this.transform) as GameObject;
+                    tempWall.transform.position = new Vector3(-1 * tileOffset, WallPrefab.transform.localScale.y / 2, y * tileOffset);
+                }
+                if (y == 0)
+                {
+                    GameObject tempWall = Instantiate(WallPrefab, this.transform) as GameObject;
+                    tempWall.transform.position = new Vector3(x * tileOffset, WallPrefab.transform.localScale.y / 2, -1 * tileOffset);
+                }
+                if (x == 9)
+                {
+                    GameObject tempWall = Instantiate(WallPrefab, this.transform) as GameObject;
+                    tempWall.transform.position = new Vector3((x + 1) * tileOffset, WallPrefab.transform.localScale.y / 2, y * tileOffset);
+                }
+                if (y == 9)
+                {
+                    GameObject tempWall = Instantiate(WallPrefab, this.transform) as GameObject;
+                    tempWall.transform.position = new Vector3(x * tileOffset, WallPrefab.transform.localScale.y / 2, (y + 1) * tileOffset);
+                }
+                Tile t = tempTile.GetComponent<Tile>() as Tile;
+                Tiles[x, y] = t;
+                //tempTile.transform.parent = this.transform;
+                tempTile.name = x.ToString() + ", " + y.ToString(); 
             }
         }
     }
