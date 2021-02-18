3f0e0e2cfb4d5f15ac585daca53b369d4185e896
Tiffany Olson <tifferzforever97@gmail.com>
2021-02-18 11:34:28 -0500

    Reading Text File; Updating Deck
    This is incomplete, but I'm attempting to display the information of a text file onto a UI. Also, I am attempting to see if how I'm using GenerateDeck and CardHandler are actually working by writing to a text file and seeing if the information displays. I believe that I have to change it "toString" somehow, but I ran out of time for now.

--- Assets/Scripts/GenerateDeck.cs
+++ Assets/Scripts/GenerateDeck.cs
@@ -1,22 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+
 
 public class GenerateDeck : MonoBehaviour
 {
-    [SerializeField]
-    public List<Card> cards = new List<Card>();
+    private CardManager cardManager;
+    private Card card;
+    private StreamWriter fileName = new StreamWriter( "ShawtyWassup.txt");
 
-    // Start is called before the first frame update
-    void Start()
+    // CardManager cardManager = new CardManager();
+
+    IEnumerator Start()
     {
-        var cardList = Resources.Load("CardList");
+        cardManager = GetComponent<CardManager>();
+        yield return new WaitForEndOfFrame();
+       
+        foreach (Card s in CardManager.cardList)
+        {
+          
+            fileName.WriteLine(s);
+            
+        }
+        fileName.Close();
+    }
+
+
+
+
+
+
+   // public void GetDeck() { }
+    
+
+  //  private  List<Card> cardList = cardManager.getCardList();
+
+}
+ /*   // Start is called before the first frame update
+    void Start(string cardList)
+    {
+    var cardList =  Resources.Load(cardList);
+    Update();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+    cardList = null;
         
     }
-}
+ */
+
