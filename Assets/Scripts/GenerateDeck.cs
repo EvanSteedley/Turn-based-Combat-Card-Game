da361c63c32d795e70f2901095e6736eeb7ae0a6
Shelby Parrish <shanneparrish@gmail.com>
2021-02-22 20:09:16 -0500

    Began Prefabs
    Don't have all card prefabs yet, so the objects are currently empty, but I tested them with what prefabs I did have and they'll load in for each of the 10 cards

--- Assets/Scripts/GenerateDeck.cs
+++ Assets/Scripts/GenerateDeck.cs
@@ -8,35 +8,41 @@ using UnityEditor;
 
 public class GenerateDeck : MonoBehaviour
 {
-    private CardManager cardManager;
-    private Card card;
-    private StreamWriter fileName = new StreamWriter( "ShawtyWassup.txt");
+    public GameObject card1;
+    public GameObject card2;
+    public GameObject card3;
+    public GameObject card4;
+    public GameObject card5;
+    public GameObject card6;
+    public GameObject card7;
+    public GameObject card8;
+    public GameObject card9;
+    public GameObject card10;
 
-    // CardManager cardManager = new CardManager();
-
-    IEnumerator Start()
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
     {
-        cardManager = GetComponent<CardManager>();
-        yield return new WaitForEndOfFrame();
-       
-        foreach (Card s in CardManager.cardList)
-        {
-          
-            fileName.WriteLine(s);
-            
-        }
-        fileName.Close();
+        // Instantiate at position (0, 0, 0) and zero rotation.
+        Instantiate(card1, new Vector3(0, 0, 0), Quaternion.identity);
+        Instantiate(card2, new Vector3(1, 0, 0), Quaternion.identity);
+        Instantiate(card3, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(card4, new Vector3(3, 0, 0), Quaternion.identity);
+        Instantiate(card5, new Vector3(4, 0, 0), Quaternion.identity);
+        Instantiate(card6, new Vector3(5, 0, 0), Quaternion.identity);
+        Instantiate(card7, new Vector3(6, 0, 0), Quaternion.identity);
+        Instantiate(card8, new Vector3(7, 0, 0), Quaternion.identity);
+        Instantiate(card9, new Vector3(8, 0, 0), Quaternion.identity);
+        Instantiate(card10, new Vector3(9, 0, 0), Quaternion.identity);
     }
 
 
 
 
 
+    // public void GetDeck() { }
 
-   // public void GetDeck() { }
-    
 
-  //  private  List<Card> cardList = cardManager.getCardList();
+    //  private  List<Card> cardList = cardManager.getCardList();
 
 }
  /*   // Start is called before the first frame update
