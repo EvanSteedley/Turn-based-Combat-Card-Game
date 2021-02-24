1972475c3b6900d44bf90d8f036ca919d7b746cd
frk94805 <63775166+frk94805@users.noreply.github.com>
2021-02-23 23:04:49 -0500

    Fixed bugs on the last commit

--- Assets/Scripts/EnemyAIzod.cs
+++ Assets/Scripts/EnemyAIzod.cs
@@ -87,134 +87,83 @@ public class EnemyAIzod : MonoBehaviour
 
     }
 
-    public void EnemyMovement()
+    
+public void EnemyMovement()
+{
+    float x1, x2, y1, y2, x3, y3, xint, yint;
+    float rx, ry, m1 = 0.0f, m2;
+    float shortestdistsq, theta = 0.0f;
+    float[] arr = { -1f, 1f };
+    float randomDir = arr[Random.Range(0, 1)];
+
+    float obstacleMinDist = 1.0f;
+    float thetaIncrement = 5.0f;
+
+    x1 = player.GetComponentInParent<Movement>().currentTile.transform.position.x;
+    y1 = player.GetComponentInParent<Movement>().currentTile.transform.position.y;
+    x2 = player.GetComponentInParent<Movement>().currentTile.transform.position.x;
+    y2 = player.GetComponentInParent<Movement>().currentTile.transform.position.y;
+
+    rx = x1 - x2;
+    ry = y1 - y2;
+
+    if (y1 == y2) theta = 0.0f;
+    else if (x1 == x2) theta = 3.1415f * 0.5f;
+    else m1 = ((y1 - y2) / (x1 - x2));
+
+    List<Tile> Obstacles = FindObjectOfType<TileMapGenerator>().Obstacles;
+
+    bool obstacleFlag = true;
+    while (obstacleFlag)
     {
-        public float x1, x2, y1, y2, x3, y3, xint, yint;
-        public float rx, ry, m1, m2;
-        public float shortestdistsq, theta;
-
-        float obstacleMinDist = 1.0;    // longest distance between obstacle and line-of-sight that blocks the line-of-sight
-        float thetaIncrement = 5.0;     // angle increment to choose new path for enemy
-
-        x1 = player.GetComponentInParent<Movement>().currentTile.transform.position.x;  // extract PLAYER x-coordinate
-        y1 = player.GetComponentInParent<Movement>().currentTile.transform.position.y;  // extract PLAYER y-coordinate
-        x2 = player.GetComponentInParent<Movement>().currentTile.transform.position.x;  // extract ENEMY x-coordinate
-        y2 = player.GetComponentInParent<Movement>().currentTile.transform.position.y;  // extract ENEMY y-coordinate
-       
-        rx = x1-x2;
-        ry = y1-y2;
-
-        bool horizontalFlag = false;    // flag to indicate horizontal line-of-sight
-        bool verticalFlag = false;      // flag to indicate vertical line-of-sight
-
-        if (y1 == y2) {         // if line-of-sight is horizontal, set theta = 0 deg
-            horizontalFlag = true;
-            if (x1 >= x2) theta = 0.0;                  
-            if (x1 < x2) theta = 3.1415;
-        }
-        else if (x1 == x2) {    // if line-of-sight is vertical, set theta = 90 deg
-            verticalFlag = true;
-            if (y1 >= y2) theta = 3.1415 * 0.5;
-            if (y1 < y2) theta = 3.1415 * 1.5;
-        }
-        else {
-            m1 = ((y1 - y2) / (x1 - x2));          // else set slope of line-of-sight = (delta y)/(delta x)
-            theta = Math.Atan(m1);                 // calculate theta from arctan of slope
-            // adjust value of theta between 0 and 360 degrees                
-            if ((y1 > y2) && (x1 > x2))
-                theta = theta;
-            if ((y1 > y2) && (x1 < x2))
-                theta = 3.1415 + theta;
-            if ((y1 < y2) && (x1 < x2))
-                theta = 3.1415 + theta;
-            if ((y1 < y2) && (x1 > x2))
-                theta = 2 * 3.1415 + theta;
-        }
-
-        public List<Tile> Obstacles;
-
-        bool obstacleFlag = true;   // flag to indicate whether obstacle blocks player-enemy line-of-sight 
-
-        // select a path and check for obstacles
-        while (obstacleFlag = true) 
+        obstacleFlag = false;
+        foreach (Tile t in Obstacles)
         {
-            obstacleFlag = false;   // re-initiate flag
 
-            // loop over list of obstacles to check if any of them blocks line-of-sight
-            foreach (Tile t in Obstacles){  
-        
-                // extract coordinates of each obstacle
-                x3 = t.transform.position.x;    
-                y3 = t.transform.position.y;
+            x3 = t.transform.position.x;
+            y3 = t.transform.position.y;
 
-                // CASE 1: line-of-sight is horizontal
-                // shortest distance between obstacle and line-of-sight is (delta y)
-                if (horizontalFlag == true) 
-                {
-                   xint = x3;
-                   yint = y2;
-                   shortestdistsq = (y3 - yint)*(y3-yint);
-                }
-
-                // CASE 2: line-of-sight is vertical
-                // shortest distance between obstacle and line-of-sight is (delta x)
-                else if (verticalFlag == true)
-                {
-                    xint = x2;
-                    yint = y3;
-                    shortestdistsq = (x3 - xint) * (x3 - xint);
-                }
-
-                // CASE 3: else
-                // find perpendicular line from obstacle location to line-of-sight
-                // find intersection point of perpendicular line with line-of-sight
-                // set (delta x) = x_intersection - x_obstacle
-                // set (delta x) = y_intersection - y_obstacle
-                // shortest distance between obstacle and line-of-sight is SQRT[(delta y)^2 + (delta x)^2]
-                else
-                {
-                    m2 = -1.0 / (m1);
-                    xint = ((y3 - (m2 * x3)) - (y2 - (m1 * x2))) / (m1 - m2);
-                    yint = (y2 + (m1 * (xint - x2)));
-                    shortestdistsq = (((x3 - xint) * (x3 - xint)) + ((y3 - yint) * (y3 - yint)));
-                }
-
-                // if shortest distance from obstacle to line-of-sight is less than the distance specified earlier
-                // then set obstacle flag to TRUE to repeat while loop with a new path
-                if (shortestdistsq <= obstacleMinDist * obstacleMinDist) obstacleFlag = true;
+            if (theta == 0.0) //case of m1 equals 0
+            {
+                xint = x3;
+                yint = y2;
+                shortestdistsq = (y3 - yint) * (y3 - yint);
             }
 
-            // if obstacle flag was TRUE (i.e. at least one obstacle blocks the line-of-sight),
-            // then randomly increment or decrement angle of previous path by THETA 
-            // check for horizontal or vertical paths
-            // re-calculate slope m1 if required
-            // and repeat the above process inside loop to determine if any obstacle blocks this new path
-            if (obstacleFlag = true) {
-                // reset horizontal and vertical path flags
-                horizontalFlag = false;
-                verticalFlag = false;
-
-                // choose a random number +1 or -1: randomDir = 
-                theta = theta + (randomDir) * (thetaIncrement * 3.1415 / 180);
-        
-                if ((theta == 0) || (theta == 3.1415)) 
-                    horizontalFlag = true;
-                else if ((theta == 3.1415 * 0.5) || (theta == 3.1415 * 1.5)) 
-                    verticalFlag = true;
-                else 
-                    m1 = Math.Tan(theta);
-
-                // if theta deviates outside [0, 360) degrees range, adjust theta to return it inside the range
-                if (theta >= 2 * 3.1415) theta = theta - 2 * 3.1415;
-                if (theta < 0) theta = 2 * 3.1415 + theta;
+            else if (theta == 3.1415 * 0.5)  //case of m1 equals infinity
+            {
+                xint = x2;
+                yint = y3;
+                shortestdistsq = (x3 - xint) * (x3 - xint);
             }
+
+            else
+            {
+                theta = Mathf.Atan(m1);
+                m2 = -1.0f / (m1);
+                xint = ((y3 - (m2 * x3)) - (y2 - (m1 * x2))) / (m1 - m2);
+                yint = (y2 + (m1 * (xint - x2)));
+                shortestdistsq = (((x3 - xint) * (x3 - xint)) + ((y3 - yint) * (y3 - yint)));
+            }
+
+            if (shortestdistsq <= obstacleMinDist * obstacleMinDist) obstacleFlag = true;
         }
 
-        // ----------------------------------------------------------
-        // Determining the tile direction where the enemy moves next
-        // ----------------------------------------------------------
-        // CASE 1: if 0<=theta<45 or 315<theta<360: move RIGHT
-        if ((theta>=0 && theta<0.25*3.1415) || (theta > 1.75*3.1415 && theta < 2 * 3.1415))
+        if (obstacleFlag)
+        {
+            // choose a random number +1 or -1: randomDir = 
+            theta = theta + (randomDir) * (thetaIncrement * 3.1415f / 180f);
+            if ((theta != 0) && (theta != 3.1415 * 0.5)) m1 = Mathf.Tan(theta);
+        }
+    }
+}
+
+
+// ----------------------------------------------------------
+// Determining the tile direction where the enemy moves next
+// ----------------------------------------------------------
+// CASE 1: if 0<=theta<45 or 315<theta<360: move RIGHT
+if ((theta>=0 && theta<0.25*3.1415) || (theta > 1.75*3.1415 && theta < 2 * 3.1415))
             // MOVE RIGHT
     
         // CASE 2: if 45<=theta<135: move UP
