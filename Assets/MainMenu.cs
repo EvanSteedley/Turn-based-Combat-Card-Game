292ef72e3ea325704e3fee55bf8b6652bf688a32
Tiffany Olson <tifferzforever97@gmail.com>
2021-03-24 15:36:42 -0400

    Adding Panel on top of Main Menu for intro effect
    This panel will display a potion bottle that will open and then display the name of the game which is "Under the Spell"

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+ 
     public void PlayGame()
     {
         StartCoroutine(GetMyRoutine());
--- Assets/PotionBottle.cs
+++ Assets/PotionBottle.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PotionBottle : MonoBehaviour
+public class PotionBottle :MonoBehaviour
+
 {
-    // Start is called before the first frame update
+    GameObject gameObject;
+
+   
+    public float sec = 5f;
+
+    
+    
     void Start()
     {
-        
+
+        StartCoroutine(RemoveAfterSeconds(5, gameObject));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator RemoveAfterSeconds(int seconds, GameObject obj)
     {
+        yield return new WaitForSeconds(2);
+        obj.SetActive(false);
+    }
+
+
+public void MoveCork()
+{
+        GameObject cork = gameObject; 
+ 
+  cork.transform.Translate(0, 1 * Time.deltaTime, 0);
+
+}
+// Update is called once per frame
+void Update()
+    {
+        print("swag");
         
     }
 }
