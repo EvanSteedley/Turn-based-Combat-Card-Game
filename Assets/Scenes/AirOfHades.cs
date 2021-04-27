9c7a5918d872680110bc0228362184881ba43085
Tiffany Olson <tifferzforever97@gmail.com>
2021-04-27 12:26:29 -0400

    Branch Updates
    Updating Branch.

---
+++ Assets/Scenes/AirOfHades.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirOfHades : AttackCard
+{
+    //TODO:  Assign the WhiteSmoke particle effect to the ProjectilePrefab.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        id = 17;
+        value = 40;
+        mana = 2;
+        name = "Air of Hades";
+        description = "Deals 40 damage to one enemy.";
+        numberOfTargets = 1;
+        Targeter = this.gameObject.GetComponent<SelectionGO>();
+        Targeter.numberOfSelections = numberOfTargets;
+        Targeter.exclusive = true;
+        SetInfo();
+    }
+}
+
+
