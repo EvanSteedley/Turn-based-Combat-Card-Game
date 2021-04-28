d633d1d067aa33481d7971b66f9f98e0301bbfff
frk94805 <63775166+frk94805@users.noreply.github.com>
2021-04-28 00:37:53 -0400

    Created new items called StunAura Item and a ManaBoost Item, created item classes, prefabs and then wired them correctly within Unity. Added the items to the Treasure List in the Sample Combat Scene

--- Assets/Scripts/Passive Items/StunAura.cs
+++ Assets/Scripts/Passive Items/StunAura.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace Assets.Scripts.Passive_Items
+public class StunAura : PassiveItem
 {
-    class StunAura
+    public Turns t;
+
+    // Start is called before the first frame update
+    void Start()
     {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Tooltip.transform.position = Input.mousePosition + new Vector3(100f, -60f, 0);
+    }
+
+    private void sceneChange(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name.Equals("Combat") || scene.name.Equals("Sample Combat"))
+        {
+            t = FindObjectOfType<Turns>();
+            t.CombatStarted += Effect;
+        }
+        else
+        {
+
+        }
+    }
+
+    private void Effect(object sender, EventArgs e)
+    {
+        foreach (Enemy enemy in t.enemies)
+        {
+            enemy.gameObject.AddComponent<Stun>();
+        }
+    }
+
+    public override void Activate()
+    {
+        SceneManager.sceneLoaded += sceneChange;
     }
 }
+
