9862b6c12db421ab6c70a8543ba4212440738f7d
frk94805 <63775166+frk94805@users.noreply.github.com>
2021-04-28 00:17:27 -0400

    Implemented the Stun status effect for the Enemy

---
+++ Assets/Scripts/Passive Items/StunAura.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Passive_Items
+{
+    class StunAura
+    {
+    }
+}
